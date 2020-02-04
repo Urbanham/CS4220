d6727228b7ee25b2a89da10a7e357a876bbe386e
cydneymikel <cydneymikel@gmail.com>
2020-02-03 19:42:58 -0800

    update examples. add lab/hw.

--- Week02/assignment.md
+++ Week02/assignment.md
@@ -5,5 +5,157 @@
 Complete the problems below. 100 Points total. Please submit lab to CSNS.
 
 - For FULL credit use proper ES6 syntax including using let/const properly.
-- For FULL credit upload only a single javascript file and output all answers using console.log().
+- For FULL credit upload only a single javascript file and output all answers to the console.
+- For Full credit do not use Regex.
+
+<hr/>
+
+## LAB
+#### 1.  Write a function called removeLetters. (10 pts)
+- The removeLetters function accepts two string arguments.
+- The first argument is a phrase and the second is a letter.
+- This function removes letter in the phrase and returns a string of the cleaned phrase.
+
+      const phrase = 'helxlo x worxxld';
+      const letter = 'x';
+      removeLetters(pharse, letter);
+
+      EXPECTED OUTPUT (string):  'hello world'
+
+
+
+#### 2.  Write a function called flattenArray. (10 pts)
+- The flattenArray function accepts a nested array as an argument.
+- This function will turn the nested array of arrays and return the flat array.
+- **DO NOT** Array.flat().
+
+      const nested = [[1, 2], [3, 4, 5], [6], [7, 8, 9]];
+      flattenArray(nested);
+
+      EXPECTED OUTPUT (array): [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+#### 3.  Write a function called adjustName. (10 pts)
+- The adjustName function accepts an object as an argument.
+- This function will remove the key 'name' and add two new keys 'firstName' and 'lastName' and return the object.
+- It will set the values of 'firstName' and 'lastName' correctly based on the values in the 'name' array.
+- You may assume that the first item in the 'name' array is the first name and that the last item is the last name.
+- Consider some people have multiple middle names so the last item in the array **MUST** be accessed dynamically.
+- **DO NOT** use iterations.
+
+
+      const dc = {
+          name: ['Bruce', 'Wayne'],
+          hero: 'Batman'
+      };
+      const marvel = {
+          name: ['Peter', 'Benjamin', 'Parker'],
+          hero: 'SpiderMan'
+      };
+      adjustName(dc);
+      EXPECTED OUTPUT (object):
+      { hero: 'Batman', firstName: 'Bruce', lastName: 'Wayne' }
+
+      adjustName(marvel);
+      EXPECTED OUTPUT (object):
+      { hero: 'SpiderMan', firstName: 'Peter', lastName: 'Parker' }
+
+<hr/>
+
+## HOMEWORK
+
+#### 4. Write a function called countingLetters. (15 pts)
+- The countingLetters function accepts a string and array arguments.
+- The return should be an object with the key as the lowercase letter and the count as the value.
+- The function should not include counts with anything from the punctions array.
+- There should not be duplicate letters in the object including counts for capital letters vs lowercase letters
+- (T and t) should just be t.
+
+        const quote = 'The greatest glory in living lies not in never falling, but in rising every time we fall.';
+        const punctuations = [' ', ',', '.', '?', '!', ';', ':', '"', "'"];
+        countingLetters(quote, punctions);
+
+        EXPECTED OUTPUT (object):
+        { t: 6,h: 1,e: 10,g: 5,r: 5,a: 3, s: 3,l: 7,o: 2,y: 2,i: 10,n: 8,v: 3,f: 2,b: 1,u: 1,m: 1,w: 1 }
+
+
+
+#### 5.Write a Function called compareArrays. (15 pts)
+- The function takes two arrays as arguments.
+- The function compares the contents of each array to determine if they are equal and return a boolean.
+- **DO NOT** us JSON.stringify()
+
+      const a1 = [1, 2, 3]
+      const a2 = [1, 2, 3]
+      compareArray(a1, a2)
+      EXPECTED OUTPUT (boolean): true
+
+      const b1 = [1, 2, 3]
+      const b2 = ['1', '2', '3']
+      compareArray(b1, b2);
+      EXPECTED OUTPUT (boolean): false
+
+
+
+#### 6.Write a Function called convertToObject. (20 pts)
+- The function takes a 2D array as an argument and returns an object.
+- Each nested array contains the key, value pair that should be added to the object.
+- The first item in the nested array always the key and the second is the value.
+
+      const game_array = [
+          [ 'developer', 'Respawn' ],
+          [ 'producer', 'EA' ],
+          [ 'game', 'Star Wars Jedi: Fallen Order' ],
+          [ 'year', 2019 ]
+      ]
+      convertToObject(game_array);
+
+      EXPECTED OUTPUT (object):
+      { developer: 'Respawn', 'producer': 'EA', game: 'Star Wars Jedi: Fallen Order', year: 2019 }
+
+
+#### 7. Write a function called buildObject. (20 pts)
+- The function takes a 2D array of objects and returns a nested object with a start key of 1.
+- Each item in the nested array contains an object with the key 'property' and the value 'assign'
+- These property and assigns are used to build key/value pairs in the return object.
+
+      const products = [[{
+          property: 'product',
+          assign: 'PS4'
+      }, {
+          property: 'company',
+          assign: 'Sony'
+      }, {
+          property: 'release',
+          assign: '2013'
+      }],
+      [{
+          property: 'product',
+          assign: 'Xbox One X'
+      }, {
+          property: 'company',
+          assign: 'Microsoft'
+      }, {
+          property: 'release',
+          assign: '2016'
+      }],
+       [{
+          property: 'product',
+          assign: 'Switch'
+      }, {
+          property: 'company',
+          assign: 'Nintendo'
+      }, {
+          property: 'release',
+          assign: '2017'
+      }]]
+
+      buildObject(products);
+
+      EXPECTED OUTPUT (object):
+      {
+        '1': { product: 'PS4', company: 'Sony', release: '2013' },
+        '2': { product: 'Xbox One X', company: 'Microsoft', release: '2016' },
+        '3': { product: 'Switch', company: 'Nintendo', release: '2017' }
+      }
 
--- Week02/examples.js
+++ Week02/examples.js
@@ -1,113 +1,167 @@
 // STRINGS
 const hello = 'Hello World it is me';
+// console.log(hello.toLowerCase());
+// console.log(hello.toUpperCase());
+// console.log(hello.includes('me'));
 
-const csv = 'hi,hello,hey,yo,greetings'
-
-console.log(splitting);
-
-
-// WHILE LOOP
-let number = 1;
-while (number < 10) {
-    console.log(number);
-    number++;
-}
-
-// FOR LOOP
-for (let number = 1; number < 10; number++) {
-    console.log(number);
-}
-
-// FOR LOOP with STRINGS
-const word = 'hello';
+const csv = 'hi,hello,hey,yo,greetings';
+const splitting = csv.split(',')
+// console.log(splitting);
 
 
+// // WHILE LOOP
+// let number = 1;
+// while (number < 10) {
+//     console.log(number);
+//     number++;
+// }
 
-// // FUNCTIONS
-// declaration notation
+// // FOR LOOP
+// for (let number = 1; number < 10; number++) {
+//     console.log(number);
+// }
+
+// // FOR LOOP with STRINGS
+// const word = 'hello';
+// for (let i = 0; i < word.length; i++) {
+//     const letter = word[i]
+//     console.log(letter)
+// }
+
+
+
+// // // FUNCTIONS
+// // declaration notation
 function addFn(a, b) {
     return a + b;
 }
-console.log(addFn(2, 3));
+// console.log(addFn(5, 2));
 
-// Functions as Value
+// // Functions as Value
+const addAsValue = function(a, b) {
+    return a + b
+}
+// console.log(addAsValue(2, 3))
 
-// Arrow Function
+// // Arrow Function
+const addArrow = (a, b) => a + b;
+// console.log(addArrow(2, 3))
 
-
-
-// // ARRAYS and METHODS
+// // // ARRAYS and METHODS
 const planets = ['venus', 'earth', 'mars'];
-console.log(planets);
-console.log(planets.length);
+// console.log(planets);
+// console.log(planets.length);
 
-// access
-const accessPlant = planets[0];
+// // access
+const accessPlant = planets[1];
+// console.log(accessPlant)
+
+// // push
+planets.push('jupitar')
+// console.log(planets.length)
+planets.push('saturn', 'uranus', 'neptune', 'pluto')
+// console.log(planets.length)
+
+// // pop
+const last = planets.pop()
+// console.log(last)
+// console.log(planets)
+
+// // unshift
+planets.unshift('mercury')
+// console.log(planets)
+
+// // shift
+const first = planets.shift()
+// console.log(first)
+// console.log(planets)
+
+// // join
+const joinPlanets = planets.join('|');
+// console.log(joinPlanets)
+
+// // includes
+const hasEarth = planets.includes('Earth')
+// console.log(hasEarth)
 
 
-// push
-
-// pop
-
-// unshift
-
-// shift
-
-// join
-
-// includes
-
-
-
-// // EXAMPLE ARRAY PROBLEM
-// Write a function called sumArray.
-// The sumArray function should accept an array as an argument and then
-// return the sum all the numbers in that array of mixed primitives.
-// sumArray(arr);  // 33
+// // // EXAMPLE ARRAY PROBLEM
+// // Write a function called sumArray.
+// // The sumArray function should accept an array as an argument and then
+// // return the sum all the numbers in that array of mixed primitives.
+// // sumArray(arr);  // 33
 const arr = [5, 7, undefined, 2, 'a', 4, true, 'b', 'c', 7, false, 8];
-const sumArray = arr => {};
+const sumArray = sumArr => {
+    let sum = 0;
+    for (let i = 0; i < sumArr.length; i++) {
+        const element = sumArr[i];
+        if (typeof element === 'number') {
+            sum = sum + element;
+        }
+    }
+
+    return sum
+};
 const ex1 = sumArray(arr);
-console.log(ex1);
+// console.log(ex1);
 
 
 
-// // EXAMPLE ARRAY and STRING PROBLEM
-// Write a function called replaceVowles.
-// This function that takes a string and an array of letters as arguments.
-// This function replaces letters in the string with a dash (-) if they are contained in the array.
-// The function return the newly created string.
-const word = 'node.js';
+// // // EXAMPLE ARRAY and STRING PROBLEM
+// // Write a function called replaceVowels.
+// // This function that takes a string and an array of letters as arguments.
+// // This function replaces letters in the string with a dash (-) if they are contained in the array.
+// // The function return the newly created string.
+const nodejs = 'node.js';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
-const replaceVowles = (word, vowels) => {};
-const ex2 = replaceVowles(word, vowels);
-console.log(ex2);
+const replaceVowels = (word, vowels) => {
+    let replaced = '';
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i];
+
+        if (vowels.includes(letter)) {
+            replaced = replaced + '-';
+        } else {
+            replaced = replaced + letter
+        }
+    }
+
+    return replaced;
+};
+const ex2 = replaceVowels(nodejs, vowels);
+// console.log(ex2);
 
 
 
-// // OBJECTS
+// // // OBJECTS
 const transformer = {
     name: 'Optimus Prime',
     team: 'Autobots',
     colors: ['red', 'blue', 'silver']
 };
-console.log(transformer);
 
-// Keys can be added using various notations
+// // Keys can be added using various notations
+transformer.homeWorld = 'Cybertron'
 
-// Keys can be removed using the delete keyword
+const type = 'vehicle'
+transformer[type] = 'truck'
+
+// // Keys can be removed using the delete keyword
+delete transformer.vehicle
+//console.log(transformer);
 
 
-
-// OBJECTS CONTINUED
+// // OBJECTS CONTINUED
 const company = {
     name: 'EA',
     type: 'Video Games',
     subsidiaries: ['BioWare', 'DICE', 'PopCap']
 };
+company.subsidiaries.push('Respawn')
+console.log(company)
+// // accessing arrays in objects
 
-// accessing arrays in objects
-
-// adding properties to object
+// // adding properties to object
 const games = [
     {
         name: 'Battlefield V',
@@ -122,15 +176,17 @@ const games = [
         release: 2019
     }
 ];
+company.games = games;
+console.log(company)
 
 
 
-// EXAMPLE OBJECT PROBLEM
-// Write a Function called convertToArray.
-// The function takes an object as an argument and returns a 2D array.
-// Each key, value pair in the object should be added to an array.
-// The first item is the key and the second is the value.
-// convertToArray(game_object)
+// // EXAMPLE OBJECT PROBLEM
+// // Write a Function called convertToArray.
+// // The function takes an object as an argument and returns a 2D array.
+// // Each key, value pair in the object should be added to an array.
+// // The first item is the key and the second is the value.
+// // convertToArray(game_object)
 // [
 //     [ 'developer', 'Respawn' ],
 //     [ 'producer', 'EA' ],
@@ -144,6 +200,16 @@ const game_obj = {
     year: 2019
 };
 
-const convertToArray = obj => {};
+const convertToArray = obj => {
+    const game_array = []
+    for (const key in obj) {
+        const pairing = [];
+        pairing.push(key)
+        pairing.push(obj[key])
+        game_array.push(pairing)
+    }
+
+    return game_array;
+};
 const game_array = convertToArray(game_obj);
 console.log(game_array);
