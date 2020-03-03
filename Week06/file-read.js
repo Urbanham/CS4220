ef8f26fefb1d3cf94409dc378e8f06e02540a27f
cydneymikel <cydneymikel@gmail.com>
2020-03-02 17:34:38 -0800

    week 06 slides and examples.

---
+++ Week06/file-read.js
@@ -0,0 +1,17 @@
+// built in node module that allows for interacting with the file system
+const fs = require('fs');
+
+// built in module that provides utilities for working with file and directory paths.
+const path = require('path');
+
+// Simple Read File
+
+
+
+// Reading Files and Taking Actions
+const files = ['large.txt', 'medium.txt', 'small.txt'];
+
+
+
+// Write Files
+
---
+++ Week06/http-request.js
@@ -0,0 +1,17 @@
+// module designed to support features of the http protocol.
+const http = require('http');
+
+
+// Simple HTTP GET Request
+
+
+// HTTP GET Request with Looping over URLs
+const urls = [
+    'http://www.google.com/',
+    'http://www.spotify.com/us/',
+    'http://twitter.com/',
+    'http://google.com/error',
+    'http://reddit.com/',
+    'http://google.com/error_again',
+    'http://www.bing.com/'
+];
